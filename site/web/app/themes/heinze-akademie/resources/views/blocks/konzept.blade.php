dbef725d585515eb8c5862e5778164a31d9ffc39
Mladen Salaj <mladensalaj@Mladens-iMac.fritz.box>
2021-06-09 18:14:54 +0200

    fix: path

--- site/web/app/themes/heinze-akademie/resources/views/blocks/konzept.blade.php
+++ site/web/app/themes/heinze-akademie/resources/views/blocks/konzept.blade.php
@@ -33,7 +33,7 @@
             Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum.
           </p>
           <div class="konzept__author-wrapper">
-            <img class="konzept__author-image" src="http://heinze-akademie.test/app/uploads/2021/06/konzept-author.jpg" alt="">
+            <img class="konzept__author-image" src="https://heinze.grand-digital.de/app/uploads/2021/06/konzept-author.jpg" alt="">
             <p class="konzept__author-testimonial">At vero eos et accusam et justo duo dolores et ea rebum.</p>
             <p class="konzept__author-name">Jan Heinze</p>
             <p class="konzept__author-position">Jobtitle</p>
@@ -41,7 +41,7 @@
         </div>
       </div>
       <div class="col-xl-4 konzept__right">
-        <img class="konzept__image" src="http://heinze-akademie.test/app/uploads/2021/06/konzept.png" alt="">
+        <img class="konzept__image" src="https://heinze.grand-digital.de/app/uploads/2021/06/konzept.png" alt="">
       </div>
     </div>
   </div>
